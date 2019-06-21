18b9819999dfd82820b4c6c803d668ea76693276
Greg Anders <greg@gpanders.com>
2019-06-20 18:05:03 -0600

    Install pytest and create unit test outline

---
+++ test/test_pushbroom.py
@@ -0,0 +1,24 @@
+import pytest
+
+import pushbroom
+
+
+@pytest.mark.xfail
+def test_dry_run():
+    assert False
+
+@pytest.mark.xfail
+def test_ignore():
+    assert False
+
+@pytest.mark.xfail
+def test_match():
+    assert False
+
+@pytest.mark.xfail
+def test_match_with_ignore():
+    assert False
+
+@pytest.mark.xfail
+def test_trash():
+    assert False
